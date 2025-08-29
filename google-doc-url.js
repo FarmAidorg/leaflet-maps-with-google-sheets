36c6f44001455dd7ab6d83bde89e439cb2d63588
farmaidmatt <matt+github@farmaid.org>
2025-08-29 09:42:31 -0700

    Update google-doc-url.js
    Changed to the new Public Form

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1L-5bbo71igXh7M2AKBlCi6RaLuakVCwzbydsCW96ReY/edit?gid=0#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1vSEZZPyti0-eOT8dBdYwizGh59Uc1deC6oNY1cgPG1A/edit?gid=0#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBDnExxf5hz3668FuTVos_It62AQTCmGJk';
